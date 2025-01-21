5ae7ccf19bcb3b19a7d1b91d28b9e988bee72e02
MeSSeR <samebody2015@gmail.com>
2025-01-21 15:18:49 +0300

    add

--- Study/GO/test.go
+++ Study/GO/test.go
@@ -5,7 +5,12 @@ import "fmt"
 func main() {
 	a := 15
 	b := 4
+	c := 12
+	d := 5
 	var x float32 = 12 / 5.0
+	var xs float32 = 15.0 / 4
 	fmt.Println(a / b)
 	fmt.Println(x)
+	fmt.Println(xs)
+	fmt.Println(c / d)
 }
